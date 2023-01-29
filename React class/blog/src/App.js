9e0e967f2eedbfdde1c371f6ca0fa4ec36d8af26
zzoo0123 <0123suh@gmail.com>
2023-01-29 19:45:30 +0900

    react class

---
+++ React class/blog/src/App.js
@@ -0,0 +1,47 @@
+/* eslint-disable */
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  // 리액트의 데이터 저장공간 state 만드는 법
+
+  // state
+  // 1. 변수 대신 쓰는 데이터 저장 공간
+  // 2. useState() 이용해 만들어야 함
+  // 장점: 웹이 app처럼 동작하게 만들고 싶음
+  // state 변경 -> HTML 자동을 재렌더링(새로고침 없이)
+  // 자주 바뀌는 중요한 데이터를 별루 말고 state로 저장해서 사용
+
+  // var [a,b] = [10, 100];
+  // [state 데이터, state 데이터 변경 함수]
+  let [title, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
+  let posts = '강남 고기 맛집'
+  let [따봉, 따봉변경] = useState(0);
+
+  return (
+    <div className="App">
+      {/*  태그에 클래스 주고 싶으면 -> <div className="클래스명"></div> */}
+      <div className="black-nav">
+        <div>개발 Blog</div>
+      </div>
+      <div className="list">
+        <h3> { title[0] } <span onClick={ () => { 따봉 + 1} }>👍🏻</span>{ 따봉 }</h3>
+        <p>1월 29일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3> { title[1] } </h3>
+        <p>1월 29일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3> { title[2] } </h3>
+        <p>1월 29일 발행</p>
+        <hr/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
